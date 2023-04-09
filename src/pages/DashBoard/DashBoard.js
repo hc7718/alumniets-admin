0ccf3b1524d70c983969e4823997e2bf171b1cc7
Harsh Chaudhary <harshchaudhary7718@gmail.com>
2023-04-09 20:41:43 +0530

    Header page creation

---
+++ src/pages/DashBoard/DashBoard.js
@@ -0,0 +1,72 @@
+import React from "react";
+import "./DashBoard.css";
+import { Button } from "@mui/material";
+import nietLogo from "../../images/nietLogo.png";
+import nietimg from "../../images/Hostel.jpg";
+const DashBoard = () => {
+  return (
+    <div>
+      <nav>
+        <div className="navbar-image-p">
+          <img className="content-items" src={nietLogo} alt="NIET LOGO"></img>
+          <p className="logo"> Alumniets</p>
+
+          <ul>
+            <li>
+              <Button
+                style={{
+                  color: "bisque",
+                }}
+                variant="text"
+              >
+                Text
+              </Button>
+            </li>
+            <li>
+              <Button
+                style={{
+                  color: "bisque",
+                }}
+                variant="text"
+              >
+                Text
+              </Button>
+            </li>
+            <li>
+              <Button
+                style={{
+                  color: "bisque",
+                }}
+                variant="text"
+              >
+                Text
+              </Button>
+            </li>
+            <li>
+              <Button
+                style={{
+                  color: "bisque",
+                }}
+                variant="text"
+              >
+                Text
+              </Button>
+            </li>
+            <li>
+              <Button
+                style={{
+                  color: "bisque",
+                }}
+                variant="text"
+              >
+                Text
+              </Button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <img src={nietimg} alt="NIET"></img>
+    </div>
+  );
+};
+export default DashBoard;
