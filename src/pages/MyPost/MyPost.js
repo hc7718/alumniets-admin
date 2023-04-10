bd453e49e84ee2ef9271be8c18c5dbf3977b5537
Harsh Chaudhary <harshchaudhary7718@gmail.com>
2023-04-11 01:56:54 +0530

    added post card

---
+++ src/pages/MyPost/MyPost.js
@@ -0,0 +1,11 @@
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+const MyPost = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+export default MyPost;
